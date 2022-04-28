0eb13c73eb6391f6dda1ca3e4a16314bbf729a8c
Elijah Smith <elijahsmith.m@gmail.com>
2022-04-27 21:26:02 -0500

    commit tree 97f8f7e2cefe112335b01621e08c570e861da272

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "brown", "nice", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "brown", "nice", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 1, "M", "orange", "nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 2, "M", "blue", "nice", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 100, "M", "yellow", "nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 8, "F", "black", "nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 4, "M", "purple", "nice", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, "F", "white", "nice", 1);
